5cf1d6e5788ebc19bdc4e085349b7a3e5f52a0d9
Daniel Joshua Anyinu <anyinu.d@gmail.com>
2022-05-22 16:35:00 +0100

    getDoc and deleteDoc

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,28 @@
+import { useAuthContext } from '../../hooks/useAuthContext'
+import {useCollection} from '../../hooks/useCollection'
+
 
 // Styles
 import styles from './Home.module.css'
 
+// Components
+import Transactionform from './Transactionform'
+import TransactionList from './TransactionList'
+
 
 function Home() {
+  const { user } = useAuthContext()
+  const {documents, error} = useCollection('transactions', ["uid", "==", user.uid], ['createdAt','desc'])
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents}/>}
+      </div>
+      <div className={styles.sidebar}>
+        <Transactionform uid={user.uid }/>
+      </div>
+    </div>
   )
 }
 export default Home
---
+++ src/pages/home/TransactionList.jsx
@@ -0,0 +1,21 @@
+// Styles
+import styles from './Home.module.css'
+import { useFirestore } from '../../hooks/useFirestore'
+
+
+function TransactionList({ transactions }) {
+  
+  const {deleteDocument} = useFirestore('transactions')
+  return (
+    <ul className={styles.transactions}>
+      {transactions.map((transaction) => (
+        <li key={transaction.id}>
+          <p className={styles.name}>{transaction.name}</p>
+          <p className={styles.amount}> ₦ {transaction.amount}</p>
+          <button onClick={()=> deleteDocument(transaction.id)}>X</button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+export default TransactionList
